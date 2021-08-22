a7eb504528ab106916b37134c4f7db51e504e638
rajan <rajanmodi30@gmail.com>
2021-08-22 17:57:25 +0530

    -added the vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <HelloWorld/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '../components/HelloWorld.vue';
 
 export default {
-  name: "Home",
+  name: 'Home',
+  
   components: {
     HelloWorld,
-  },
-  methods: {
-    getToken() {
-      // const messaging = firebase.messaging();
-      // messaging.getToken({ vapidKey: "BKagOny0KF_2pCJQ3m....moL0ewzQ8rZu" });
-    },
   },
 };
 </script>
