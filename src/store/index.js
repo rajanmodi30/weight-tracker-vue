7bef9db6bfaf83bf037566b0985ba51cd4df4355
rajan <rajanmodi30@gmail.com>
2021-08-22 18:04:09 +0530

    -added the modules for vuex and the vuex persistant storage mudle

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,15 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import auth from "./modules/auth";
+import weight from "./modules/weight";
+import VuexPersistence from "vuex-persist";
 
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+});
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
-  }
-})
+    auth,
+    weight,
+  },
+  plugins: [vuexLocal.plugin],
+});
---
+++ src/store/modules/auth.js
@@ -0,0 +1,14 @@
+const state = {};
+const getters = {};
+const actions = {};
+const mutations = {};
+
+const auth = {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
+
+export default auth;
