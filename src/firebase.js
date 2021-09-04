847c062d45d226815850cca4612d965e9992b6a5
rajan <rajanmodi30@gmail.com>
2021-09-04 15:22:29 +0530

    -proper env configuration

--- src/firebase.js
+++ src/firebase.js
@@ -5,18 +5,17 @@ import { getMessaging } from "firebase/messaging";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCLjNFTPgV_pC1acsakn12odDVdl34UupA",
-  authDomain: "weight-tracker-62503.firebaseapp.com",
-  projectId: "weight-tracker-62503",
-  storageBucket: "weight-tracker-62503.appspot.com",
-  messagingSenderId: "719588529069",
-  appId: "1:719588529069:web:8798fcea60a4f889bfc724",
-  measurementId: "G-W9HZV4PDQE",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_Id,
 };
 
 const app = initializeApp(firebaseConfig);
 
 const messaging = getMessaging(app);
 
-// export _default { messaging };
 export default messaging;
--- src/services/axiosManage.js
+++ src/services/axiosManage.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: process.env.API_KEY,
   withCredentials: false,
   headers: {
     Accept: "application/json",
