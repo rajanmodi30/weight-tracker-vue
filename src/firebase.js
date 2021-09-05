ed6b8bfb1dac600c826f2da5cccd17ade605aa88
rajan <rajanmodi30@gmail.com>
2021-09-05 10:13:16 +0530

    -testing the fcm

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getMessaging, getToken } from "firebase/messaging";
+import { getMessaging } from "firebase/messaging";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
