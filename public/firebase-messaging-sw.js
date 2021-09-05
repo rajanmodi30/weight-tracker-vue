a389247a48f92d32afab3bae553ee44aa19d68ef
rajan <rajanmodi30@gmail.com>
2021-09-05 18:32:58 +0530

    -service worker changes

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 
-importScripts('https://www.gstatic.com/firebasejs/8.4.3/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.4.3/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.1/firebase-messaging.js');
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
