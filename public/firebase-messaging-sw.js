f75ff9745bd1a8573a0fd5ba1d0f14e579c3669d
rajan <rajanmodi30@gmail.com>
2021-09-05 18:40:38 +0530

    -fireabse service worker changes

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,18 +1,18 @@
 // Import the functions you need from the SDKs you need
 
-importScripts('https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.1/firebase-messaging.js');
+importScripts("https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/9.0.1/firebase-messaging.js");
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCLjNFTPgV_pC1acsakn12odDVdl34UupA",
-    authDomain: "weight-tracker-62503.firebaseapp.com",
-    projectId: "weight-tracker-62503",
-    storageBucket: "weight-tracker-62503.appspot.com",
-    messagingSenderId: "719588529069",
-    appId: "1:719588529069:web:8798fcea60a4f889bfc724",
-    measurementId: "G-W9HZV4PDQE",
-  };
+  apiKey: "AIzaSyCLjNFTPgV_pC1acsakn12odDVdl34UupA",
+  authDomain: "weight-tracker-62503.firebaseapp.com",
+  projectId: "weight-tracker-62503",
+  storageBucket: "weight-tracker-62503.appspot.com",
+  messagingSenderId: "719588529069",
+  appId: "1:719588529069:web:8798fcea60a4f889bfc724",
+  measurementId: "G-W9HZV4PDQE",
+};
 
-const app = firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
