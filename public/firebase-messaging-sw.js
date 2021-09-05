30aaaede37491a1dccea0e3edbed1cd5f742a7a2
rajan <rajanmodi30@gmail.com>
2021-09-05 10:00:47 +0530

    -added firebase service worker

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+
+importScripts('https://www.gstatic.com/firebasejs/8.4.3/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.4.3/firebase-messaging.js');
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_Id,
+};
+
+const app = firebase.initializeApp(firebaseConfig)
